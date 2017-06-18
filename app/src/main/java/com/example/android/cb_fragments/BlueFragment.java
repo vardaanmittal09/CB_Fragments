e19ea51fbd026a96e5aa263177b746284cd96a09
lenovo <vardaanmittal09@gmail.com>
2017-06-18 12:55:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/cb_fragments/BlueFragment.java
@@ -0,0 +1,30 @@
+package com.example.android.cb_fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlueFragment extends Fragment {
+
+
+    public BlueFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView=inflater.inflate(R.layout.fragment_blue,container,false);
+        return  rootView;
+    }
+
+}
